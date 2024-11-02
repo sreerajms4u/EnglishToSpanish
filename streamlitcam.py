4a454cee3ee91a304253f22470ba46a7d2a15eb0
Sreeraj Surendran <sreeraj.surendran@benefitalign.com>
2024-11-02 11:49:24 -0500

    New changes for the Story

--- streamlitcam.py
+++ streamlitcam.py
@@ -2,6 +2,7 @@
 #python -m streamlit run cam.py
 import cv2
 import streamlit as st
+from transformers import pipeline
 
 imageName="Sreeeraj.jpg"
 
@@ -23,7 +24,19 @@ def capture_image():
         with col2:
             st.header("Picture")
             FRAME_IMGWINDOW.image(imageName)
-
+def img2text():
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text=image_to_text(
+        imageName)[0]['generated_text']
+    print(text)
+    st.text(generate_story(text))
+def generate_story(scenario):
+    generator = pipeline("text-generation", model="gpt2")
+    result = generator(scenario, max_length=200, num_return_sequences=1)
+    return result[0]["generated_text"]
+def drawBtn_GenerateStory():
+    st.button("Generate Story", on_click=img2text)
+   
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
@@ -33,6 +46,7 @@ with col1:
     FRAME_VIDWINDOW = st.image([])
     st.header("Video")
     drawBtn()
+    drawBtn_GenerateStory()
     while run:
         _, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
